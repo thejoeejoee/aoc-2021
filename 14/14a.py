655e4a9966f9e25b800f5719bb1a4c0489c2fd17
Josef Kolář <thejoeejoee@gmail.com>
2021-12-14 10:41:59 +0100

    14, lanternfish again? always has been

---
+++ 14/14a.py
@@ -0,0 +1,39 @@
+#!/bin/env python3
+import operator
+from operator import attrgetter, itemgetter
+from collections import defaultdict, Counter
+from functools import reduce, partial
+from itertools import chain
+from pprint import pprint
+
+from aocd import get_data
+
+
+def windowed(seq, n):
+    for i in range(len(seq) - n + 1):
+        yield seq[i: i + n]
+
+
+def main(lines: list[str]):
+    loader = iter(lines)
+    word = list(next(loader))
+
+    rules = {
+        line[0:2]: line[6]
+        for line in lines[2:]
+    }
+
+    for _ in range(10):
+        inserts = []
+        for i, w in enumerate(windowed(word[:], 2)):
+            inserts.append((i + 1, rules.get(''.join(w)) or ''))
+
+        for i, what in inserts[::-1]:
+            word.insert(i, what)
+
+    c = Counter(word)
+    return max(c.values()) - min(c.values())
+
+
+if __name__ == '__main__':
+    print(main(lines=get_data().strip().splitlines()))
---
+++ 14/14b.py
@@ -0,0 +1,53 @@
+#!/bin/env python3
+from collections import Counter
+
+from aocd import get_data
+
+
+def windowed(seq, n):
+    for i in range(len(seq) - n + 1):
+        yield seq[i: i + n]
+
+
+def main(lines: list[str]):
+    loader = iter(lines)
+    word = Counter(windowed(initial := next(loader), 2))
+    ends = {initial[0], initial[-1]}
+
+    rules = {
+        line[0:2]: line[6]
+        for line in lines[2:]
+    }
+
+    for _ in range(40):
+        for pair, count in tuple(word.items()):
+            if not count:
+                continue
+            if not (to_insert := rules.get(pair)):
+                continue
+
+            left, right = pair
+            # cannot be merged since it could contain duplicities
+            word.update({pair: -1 * count})
+            word.update({f'{left}{to_insert}': 1 * count})
+            word.update({f'{to_insert}{right}': 1 * count})
+
+    c = Counter(ends)
+    for pair, count in word.items():
+        c.update({pair[0]: count})
+        c.update({pair[1]: count})
+
+    def correct(v: int) -> int:
+        if v == 1:
+            return 1
+        elif v % 2 == 0:
+            return v // 2
+        raise ValueError('Should not happen')
+
+    counts = tuple(map(correct, c.values()))
+
+    return max(counts) - min(counts)
+
+
+if __name__ == '__main__':
+    print(main(lines=get_data().strip().splitlines()))
